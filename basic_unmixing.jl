56c0d9ab57fdcb55421c3b442aeec5a4d3ed196a
antoinelerosey <73841545+antoinelerosey@users.noreply.github.com>
2020-11-18 20:32:40 +0100

    trying to commit

--- basic_unmixing.jl
+++ basic_unmixing.jl
@@ -16,32 +16,36 @@ function initialize_abundance(n, p, n_constraints)
         A[:, j] = rand(uniform_distribution, p)
         if n_constraints == 2
             sum_row = sum(A[:, j])
-            A[:, j] = map(x -> x / sum_row, A[:, j])
+            A[:, j] = A[:, j] / sum_row
         end
     end
     return A
 end
 
 function abundance_estimation(X, S, nb_constraints)
-    max_iter = 10e2
-    precision = 10e-8
+    precision_estimation = 270
+    max_iter_estimation = 100
+    
+    precision_projection = 0.001
+    max_iter_projection = 300
+
     # The main stepsize is set to twice the reciprocal of the smallest
     # Lipschitz constant of the gradient
-    main_stepsize = LinearAlgebra.opnorm(S' * S)
-    projection_stepsize = 0.01
-    # Initialize an abundance matrix
     n, p = size(X)[2], size(S)[2]
+    main_stepsize = LinearAlgebra.opnorm(S' * S) / n
+    projection_stepsize = 0.05
+    # Initialize an abundance matrix
     main_A = initialize_abundance(n, p, nb_constraints)
 
     loss = [get_distance(X, S * main_A)]
     main_iter = 0
-    while main_iter < max_iter
+    while main_iter < max_iter_estimation && get_distance(X, S * main_A) > precision_estimation
         main_∇f = S' * (S * main_A - X)
         main_A -= main_stepsize * main_∇f
 
         projection_A = initialize_abundance(n, p, nb_constraints)
         projection_iter = 0
-        while projection_iter < max_iter
+        while projection_iter < max_iter_estimation && get_distance(main_A, projection_A) > precision_projection
             projection_∇f = projection_A - main_A
             projection_A -= projection_stepsize * projection_∇f
             projection_iter += 1
